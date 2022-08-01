4033a00c6f281244c33091037325edc125aec07d
Jonas Villacis <jonasvillacis@yahoo.com>
2022-08-01 10:18:43 -0500

    Se agrego el home

---
+++ grupo_3_b/lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final menuOptions = AppRoutes.menuOptions;
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: ListView.separated(
+          itemBuilder: (context, index) => ListTile(
+                leading: Icon(menuOptions[index].icon),
+                title: Text(menuOptions[index].name),
+                onTap: () {
+                  Navigator.pushNamed(context, menuOptions[index].route);
+                },
+              ),
+          separatorBuilder: (_, __) => const Divider(),
+          itemCount: menuOptions.length),
+    );
+  }
+}
--- grupo_3_b/lib/screens/screens.dart
+++ grupo_3_b/lib/screens/screens.dart
@@ -1,1 +1,2 @@
 export 'package:grupo_3_b/screens/empresa_list_screen.dart';
+export 'package:grupo_3_b/screens/empresa_form_screen.dart';
