1abef2b52f1df336a40ddcbea74c86432e3d8e2a
Jonas Villacis <jonasvillacis@yahoo.com>
2022-08-08 09:24:10 -0500

    Home menu cambiado

--- grupo_3_b/lib/screens/home_screen.dart
+++ grupo_3_b/lib/screens/home_screen.dart
@@ -10,16 +10,18 @@ class HomeScreen extends StatelessWidget {
     final menuOptions = AppRoutes.menuOptions;
     return Scaffold(
       appBar: AppBar(title: const Text('Home Screen')),
-      body: ListView.separated(
-          itemBuilder: (context, index) => ListTile(
-                leading: Icon(menuOptions[index].icon),
-                title: Text(menuOptions[index].name),
-                onTap: () {
-                  Navigator.pushNamed(context, menuOptions[index].route);
-                },
-              ),
-          separatorBuilder: (_, __) => const Divider(),
-          itemCount: menuOptions.length),
+      drawer: Drawer(
+          child: ListView.separated(
+              itemBuilder: (context, index) => ListTile(
+                    leading: Icon(menuOptions[index].icon),
+                    title: Text(menuOptions[index].name),
+                    onTap: () {
+                      Navigator.pushNamed(context, menuOptions[index].route);
+                    },
+                  ),
+              separatorBuilder: (_, __) => const Divider(),
+              itemCount: menuOptions.length)),
+      body: const Text('Hola'),
     );
   }
 }
