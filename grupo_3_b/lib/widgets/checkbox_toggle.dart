9b681cd19e8375b2e567e75c6db55f9ebe296b31
Jonas Villacis <jonasvillacis@yahoo.com>
2022-08-09 11:12:25 -0500

    Peobando con stateful

--- grupo_3_b/lib/widgets/checkbox_toggle.dart
+++ grupo_3_b/lib/widgets/checkbox_toggle.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CheckboxToggle extends StatelessWidget {
+class CheckboxToggle extends StatefulWidget {
   final Map<String, dynamic> formValues;
   final String formProperty;
 
@@ -9,10 +9,24 @@ class CheckboxToggle extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<CheckboxToggle> createState() => _CheckboxToggleState();
+}
+
+class _CheckboxToggleState extends State<CheckboxToggle> {
+  bool isSwitched = false;
+
+  @override
   Widget build(BuildContext context) {
     return Switch(
-      value: formValues[formProperty],
-      onChanged: (value) => formValues[formProperty] = value,
+      value: isSwitched,
+      onChanged: (value) {
+        setState(() {
+          isSwitched = value;
+          print(isSwitched);
+        });
+      },
+      activeTrackColor: Colors.lightGreenAccent,
+      activeColor: Colors.green,
     );
   }
 }
--- grupo_3_b/lib/widgets/input_date_picker.dart
+++ grupo_3_b/lib/widgets/input_date_picker.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 
-class InputDatePickerField extends StatelessWidget {
+class InputDatePickerField extends StatefulWidget {
   final Map<String, dynamic> formValues;
   final String formProperty;
-
-  const InputDatePickerField(
+  InputDatePickerField(
       {Key? key, required this.formValues, required this.formProperty})
       : super(key: key);
 
   @override
+  State<InputDatePickerField> createState() => _InputDatePickerFieldState();
+}
+
+class _InputDatePickerFieldState extends State<InputDatePickerField> {
+  @override
   Widget build(BuildContext context) {
+    DateTime selectedDate = DateTime(2022, 12, 12);
     return InputDatePickerFormField(
-      firstDate: DateTime(1900, 1, 1),
-      lastDate: DateTime(3000, 1, 1),
-      initialDate: formValues[formProperty],
-      onDateSubmitted: (value) => formValues[formProperty] = value,
-    );
+        firstDate: DateTime(2019),
+        lastDate: DateTime(2020, 12, 12),
+        initialDate: selectedDate,
+        onDateSubmitted: (date) {
+          setState(() {
+            selectedDate = date;
+          });
+        });
   }
 }
