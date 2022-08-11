cb720f787c6a1e4a7ecb38444887ae2033b3d9f7
Jonas Villacis <jonasvillacis@yahoo.com>
2022-08-11 09:13:36 -0500

    Se corrigio los errores de las exportaciones

--- grupo_3_b/lib/widgets/widgets.dart
+++ grupo_3_b/lib/widgets/widgets.dart
@@ -1,4 +1,6 @@
 export 'package:grupo_3_b/widgets/checkbox_toggle.dart';
-
 export 'package:grupo_3_b/widgets/custom_input_field.dart';
+export 'package:grupo_3_b/widgets/input_date_picker.dart';
+export 'package:grupo_3_b/widgets/custom_navigator_bottom.dart';
+export 'package:grupo_3_b/widgets/alert_dialog.dart';
 export 'package:grupo_3_b/widgets/custom_form.dart';
