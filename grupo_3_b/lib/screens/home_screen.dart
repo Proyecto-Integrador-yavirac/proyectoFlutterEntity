dfe402117df1d0bd5a5d68210001bed8f75af3dc
AnthonyFabian212 <fabico-2002@outlook.com>
2022-08-15 17:14:10 -0500

    Prueba de Navegacion

--- grupo_3_b/lib/screens/home_screen.dart
+++ grupo_3_b/lib/screens/home_screen.dart
@@ -9,55 +9,46 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final menuOptions = AppRoutes.menuOptions;
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 254, 255, 255),
-        appBar: AppBar(
-          title: const Text('Empresas'),
-          flexibleSpace: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 92, 96, 185),
-                  Color.fromARGB(255, 2, 28, 90)
-                ],
-                begin: Alignment.bottomRight,
-                end: Alignment.topLeft,
-              ),
+      backgroundColor: const Color.fromARGB(255, 254, 255, 255),
+      appBar: AppBar(
+        title: const Text('Empresas'),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 92, 96, 185),
+                Color.fromARGB(255, 2, 28, 90)
+              ],
+              begin: Alignment.bottomRight,
+              end: Alignment.topLeft,
             ),
           ),
-          elevation: 20,
-          titleSpacing: 20,
         ),
-        body: Container(
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3),
-            itemBuilder: (context, index) {
-              return InkWell(
-                onTap: () {
-                  Navigator.pushNamed(context, menuOptions[index].route);
-                },
-                child: Container(
-                    margin: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(152, 41, 5, 119),
-                      borderRadius: BorderRadius.circular(2),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      verticalDirection: VerticalDirection.down,
-                      children: [
-                        Text(
-                          menuOptions[index].name,
-                          style: const TextStyle(
-                            color: Color.fromARGB(255, 246, 245, 245),
-                          ),
-                        ),
-                      ],
-                    )),
-              );
-            },
-            itemCount: menuOptions.length,
+        elevation: 20,
+        titleSpacing: 20,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
-        ));
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
